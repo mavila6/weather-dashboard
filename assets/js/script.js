8ca18d6683634f9a3389291e544a6cf1a42bb0a7
Marco Avila <mavila6@email.arizona.com>
2021-09-18 10:10:39 -0700

    added function to dynamically create p elements for searched cities and add them to the page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 // Global variables
 const apiKey = 'f4e46f6e668c802af0710e241b819bb8';
-const city = $(".textbox").val().trim();
+const searches = $(".savedSearch");
 // Empty array to store searches and undefined variables to be used later
 let history = [];
 let latitude;
@@ -9,16 +9,30 @@ let longitude;
 $("#search").on('click', e => {
     // Prevents refresh on button click
     e.preventDefault();
-    // Adds searched city to the history array and saves array to local storage
-    history.pushState(city);
-    localStorage.setItem("history", JSON.stringify(history));
-    
+    const city = $(".textbox").val().trim();
+    // Adds searched city to the history array, saves array to local storage, clears the input, alerts if no value is searched for, & makes calls to the appropriate functions
+    if (city) {
+        history.push(city);
+        localStorage.setItem("history", JSON.stringify(history));
+        $(".textbox").val("");
+        displayCity();
+    } else {
+        alert("Must enter a city first!")
+    }
+    //localStorage.clear();
 });
 
 // Added event listener using JQuery to saved cities 
 
-// Function to display the saved searches
-
+// Function to dynamically create elements for each searched city and display them on the page 
+function displayCity() {
+    searches.empty();
+    for (let i = 0; i < history.length; i++) {
+        let pEl = element = $("<p>").text(history[i]);
+        pEl.addClass("cities");
+        searches.prepend(pEl);
+    }
+}
 // Function used to call OpenWeather API and receive weather data/append data to the page
 
 // Function that changes the UV color based on conditions
